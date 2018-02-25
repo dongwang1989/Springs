a9f86c40a85aea374c34deadfd7feb308da7c211
dongwang1989 <1819591996@qq.com>
2018-02-25 15:56:10 +0800

    6

--- spring-boot/src/main/java/cn/zzdz/login/Login.java
+++ spring-boot/src/main/java/cn/zzdz/login/Login.java
@@ -10,28 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Login {
-	/*@RequestMapping(value="/login",method=RequestMethod.POST) // @PathVariable String username,@PathVariable String pwd
-	public String log(@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "pwd", required = true) String pwd, HttpSession session) {
-		String values;
-		Object strses = session.getAttribute("username");
-		if (strses != null && !strses.equals("")) {
-			values = "当前账号" + session.getAttribute("username") + "已经登陆！";
-		} else {
-			System.out.println("aaa" + username + pwd);
-			if (username.equals("zhangsan") && pwd.equals("123")) {
-				System.out.println("succe");
-				values = "登陆成功";
-				session.setAttribute("username", username);
-			} else {
-				values = "登陆error";
-			}
-		}
-		return values;
-	}*/
-	@RequestMapping(value="/login",method=RequestMethod.POST) // @PathVariable String username,@PathVariable String pwd
-	public String log(@RequestBody String username,
-			@RequestBody String pwd, HttpSession session) {
+	@RequestMapping(value = "/login", method = RequestMethod.POST) // @PathVariable String username,@PathVariable String															// pwd
+	public String log(@RequestBody String username, @RequestBody String pwd, HttpSession session) {
 		String values;
 		Object strses = session.getAttribute("username");
 		if (strses != null && !strses.equals("")) {
