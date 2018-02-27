70cf9c284e28d7d0ab96a83501fd77b5bcfa1d05
dongwang1989 <1819591996@qq.com>
2018-02-27 14:08:16 +0800

    1

--- spring-boot/src/main/java/cn/zzdz/Run.java
+++ spring-boot/src/main/java/cn/zzdz/Run.java
@@ -3,7 +3,7 @@ package cn.zzdz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // (exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication 
 public class Run {
 	public static void main(String[] args) {
 		SpringApplication.run(Run.class, args);
