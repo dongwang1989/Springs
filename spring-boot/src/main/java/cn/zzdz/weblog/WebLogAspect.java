5d14fc73afb73c784c7f0477b1a9c73ae518c5e2
dongwang1989 <1819591996@qq.com>
2018-03-08 13:35:02 +0800

    123

--- spring-boot/src/main/java/cn/zzdz/weblog/WebLogAspect.java
+++ spring-boot/src/main/java/cn/zzdz/weblog/WebLogAspect.java
@@ -82,4 +82,16 @@ public class WebLogAspect {
 		logger.info("WebLogAspect.doAfterReturning()");
 
 	}
+	// @Around("logsta()")
+	/*
+	 * public Object aroundStatus(ProceedingJoinPoint pjp) throws Throwable { Object
+	 * proceed; System.out.println("这是环绕通知之前的部分!!"); // 获取将要执行的方法名称 String
+	 * methodName = pjp.getSignature().getName();
+	 * 
+	 * HttpServletRequest request = ((ServletRequestAttributes)
+	 * RequestContextHolder.getRequestAttributes()) .getRequest(); HttpSession
+	 * session = request.getSession(); if (session.getAttribute("username") != null)
+	 * { proceed = pjp.proceed();// 调用目标方法 } else { proceed="403"; }
+	 * System.out.println("这是环绕通知之后的部分!!"); return proceed; }
+	 */
 }
